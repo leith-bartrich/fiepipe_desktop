09eab3d91956ffb94c11d53c917a1995c7dc6718
Bradley Friedman <brad@fie.us>
2019-03-29 13:43:52 -0400

    Removed local site shell and added container shell directly to registered legal entity shell. Fixed up structure inheritence to make some sense.  Generics were all broken prior.  A lot of documentation added too. Added automanage command directly to registered legal entity shell.

--- fiepipedesktoplib/legalentity/registry/shell/legal_entity.py
+++ fiepipedesktoplib/legalentity/registry/shell/legal_entity.py
@@ -6,14 +6,19 @@ import fiepipelib.localuser.routines.localuser
 import fiepipedesktoplib.shells.AbstractShell
 import fiepipedesktoplib.sites.shells.localsite
 from fiepipelib.legalentity.registry.data.registered_entity import localregistry, RegisteredEntity
-
+from fiepipedesktoplib.shells.variables.fqdn_var_command import FQDNVarCommand
+from fiepipedesktoplib.container.shells.manager import ContainerManagerCommand
+from fiepipelib.automanager.routines.automanager import AutoManagerInteractiveRoutines
 
 class LegalEntityShell(fiepipedesktoplib.shells.AbstractShell.AbstractShell):
     """Shell for working within a legal entity."""
 
     _localUser = None
-    _fqdn = None
 
+    _fqdn_var_command: FQDNVarCommand = None
+
+    def get_fqdn(self) -> str:
+        return self._fqdn_var_command.get_value()
 
     def get_plugin_names_v1(self) -> typing.List[str]:
         ret = super().get_plugin_names_v1()
@@ -21,14 +26,14 @@ class LegalEntityShell(fiepipedesktoplib.shells.AbstractShell.AbstractShell):
         return ret
 
     def get_prompt_text(self) -> str:
-        return self.prompt_separator.join(["fiepipe", self._fqdn])
+        return self.prompt_separator.join(["fiepipe", self.get_fqdn()])
 
     def _getEntity(self) -> RegisteredEntity:
         reg = localregistry(self._localUser)
-        entities = reg.GetByFQDN(self._fqdn)
+        entities = reg.GetByFQDN(self.get_fqdn())
         if len(entities) == 0:
             self.perror("No such entity.")
-            raise LookupError("No such entity: " + self._fqdn)
+            raise LookupError("No such entity: " + self.get_fqdn())
         entity = entities[0]
         return entity
 
@@ -36,77 +41,32 @@ class LegalEntityShell(fiepipedesktoplib.shells.AbstractShell.AbstractShell):
         assert isinstance(localUser, fiepipelib.localuser.routines.localuser.LocalUserRoutines)
         self._localUser = localUser
         self._fqdn = fqdn
+
+        self._fqdn_var_command = FQDNVarCommand(fqdn)
+        self.add_variable_command(self._fqdn_var_command, "fqdn", [], False)
+
         super().__init__()
 
+        self.add_submenu(ContainerManagerCommand(
+            self._fqdn_var_command), "containers", ['cnt'])
 
-    #    def do_list_networked_sites(self, arg):
-    #        registry = fiepipelib.networkedsite.localregistry(self._localUser)
-    #        entity = self._getEntity()
-    #        sites = registry.GetByFQDN(self._fqdn)
-    #        for s in sites:
-    #            assert isinstance(s, fiepipelib.networkedsite.networkedsite)
-    #            print(s.GetName())
-
-    #   def do_delete_networked_site(self,arg):
-    #       """Deletes the named networked site.
-    #       Usage: delete_networked_site [sitename]
-    #       arg sitename: the name of the site to delete
-    #       """
-    #       if arg == None:
-    #           print("No sitename specified.")
-    #           return
-    #       if arg == "":
-    #           print("No sitename specified.")
-    #           return
-    #       registry = fiepipelib.networkedsite.localregistry(self._localUser)
-    #       fqdn = self._fqdn
-    #       registry.DeleteByFQDNAndName(fqdn,arg)
-
-    #    def do_set_networked_site(self, arg):
-    #        raise NotImplementedError()
-
-    def do_local_site_shell(self, arg):
-        """Runs a subshell for working with the local site
-        Usage: set_local_site"""
-        fiepipedesktoplib.sites.shells.localsite.Shell(self._localUser, self._getEntity()).cmdloop()
-
-    def do_lssh(self, arg):
-        """Alias for set_local_site"""
-        self.do_local_site_shell(arg)
 
     def GetForkArgs(self):
-        return [self._fqdn]
+        return [self.get_fqdn()]
 
-    # def do_add_networked_site(self,arg):
-    #    """Allows the manual addition of a site.
-    #    Usage: add_site [fqdn] [sitename] [stateserver]...
-    #    arg fqdn: The fully qualified domain name of the legal enity to create a site in.
-    #    arg sitename: The name of the site to add.
-    #    arg stateserver: 1 or more servers to log into to get the state of the site.  They should be listed in failover order with the first being the primary.
-    #    Example: add_site mysite.com global fiepipe1.mysite.com fiepipe2.mysite.com
-    #    """
-    #    if arg == None:
-    #        print ("No arguments specified.")
-    #        return
-    #    if arg == "":
-    #        print ("No arguments specified.")
-    #        return
-    #    args = arg.split(1)
-    #    if len(args) != 2:
-    #        print ("No sitename specified.")
-    #        return
-    #    fqdn = args[0]
-    #    args = args[1].split(1)
-    #    if len(args) != 2:
-    #        print("No stateserver specified")
-    #        return
-    #    sitename = args[0]
-    #    stateServers = args[1].split()
-    #    site = fiepipelib.networkedsite.FromParameters(sitename,fqdn,stateServers)
-    #    registery = fiepipelib.siteregistry.siteregistry(self._localUser)
-    #    registery.SetNetworkedSite(site)
-    #    if isinsatnce(self, fiepipedesktoplib.shells.networkedsite.Shell):
-    #       self.do_reload(None)
+    def do_automanage(self, args):
+        """Runs the automanager for this legal entity, if configured.
+        Usage: automanage
+        """
+        args = self.parse_arguments(args)
+        automanager_routines = AutoManagerInteractiveRoutines(0.0)
+        try:
+            config = automanager_routines.get_legal_entitiy_config(self.get_fqdn())
+        except KeyError as err:
+            self.perror("This entity isn't configured for automanager.")
+            return
+        self.do_coroutine(automanager_routines.automanage_fqdn(self.get_feedback_ui(),config))
+
 
 
 if __name__ == "__main__":
